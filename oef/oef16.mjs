37ed96950ca139be605badef8d78e656fedfff91
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-19 09:59:42 +0100

    oef16

---
+++ oef/oef16.mjs
@@ -0,0 +1,15 @@
+/*function naam(parameterlijst){
+    codeblok;
+}
+*/
+
+function som(num1, num2){
+    let som = num1 + num2;
+    console.log(`de som van ${num1} en ${num2} is ${som}.`);
+}
+
+som(3, 9);
+som(12,900342);
+som(-389872, 728301);
+
+process.exit();
